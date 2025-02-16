7c7e5c404480a10b6c8475915a5fd67b6f5544a8
Jackson <gidykamau@gmail.com>
2025-02-16 12:04:35 +0300

    Fixed UserProfile.jsx to include 'Bio' keyword

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,10 +3,10 @@ const UserProfile = (props) => {
       <div className="user-profile">
         <h2>{props.name}</h2>
         <p>Age: {props.age}</p>
-        <p>{props.bio}</p>
+        <p>Bio: {props.bio}</p> {/* Make sure 'Bio' is included explicitly */}
       </div>
     );
   };
   
   export default UserProfile;
-   
+  
