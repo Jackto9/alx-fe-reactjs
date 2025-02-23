42d27b69adddd28f0a6e6ee2bd90f6118df8a8ca
Jackson <gidykamau@gmail.com>
2025-02-23 17:03:18 +0300

    Added inline CSS styling to all components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+const MainContent = () => {
+    return (
+      <main style={{ 
+        padding: '20px', 
+        backgroundColor: '#e0f7fa', 
+        color: '#00796b' 
+      }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    );
+  };
+  
+  export default MainContent;
+  
