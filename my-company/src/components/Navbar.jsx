53c1c7d847bd9d72953174e6159a1106589daa11
Jackson <gidykamau@gmail.com>
2025-02-24 00:20:53 +0300

    Add flex display and justifyContent to Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,11 +3,26 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "#f0f0f0" }}>
-      <Link to="/" style={{ margin: "0 10px" }}>Home</Link>
-      <Link to="/about" style={{ margin: "0 10px" }}>About</Link>
-      <Link to="/services" style={{ margin: "0 10px" }}>Services</Link>
-      <Link to="/contact" style={{ margin: "0 10px" }}>Contact</Link>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "10px",
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <Link to="/" style={{ textDecoration: "none" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ textDecoration: "none" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ textDecoration: "none" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ textDecoration: "none" }}>
+        Contact
+      </Link>
     </nav>
   );
 }
