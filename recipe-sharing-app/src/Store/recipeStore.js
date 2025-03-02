3b32a9f8389d4e84b2ccff8c50fd16ce4a10551b
Jackson <gidykamau@gmail.com>
2025-03-02 22:47:49 +0300

    Save changes before pulling latest updates

--- recipe-sharing-app/src/Store/recipeStore.js
+++ recipe-sharing-app/src/Store/recipeStore.js
@@ -2,7 +2,25 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  // Add a new recipe
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe],
+  })),
+
+  // Update an existing recipe
+  updateRecipe: (id, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+    ),
+  })),
+
+  // Delete a recipe
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
+  })),
+
+  // Set initial recipes
   setRecipes: (recipes) => set({ recipes }),
 }));
 
