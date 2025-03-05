4f27cd6b57f1260614aa5c10ec9f5d70b41c8a03
Jackson <gidykamau@gmail.com>
2025-03-05 21:32:23 +0300

    Initial commit - Form Handling in React

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/, // Apply JSX processing for .js files in src
+  },
+});
