9ee4a41b74129bb75398410b5af2c7b462c2b544
Jackson <gidykamau@gmail.com>
2025-03-07 13:30:45 +0300

    Add Profile component with nested routes

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <nav>
+        <ul>
+          <li><Link to="details">Profile Details</Link></li>
+          <li><Link to="settings">Profile Settings</Link></li>
+        </ul>
+      </nav>
+      <Outlet /> {/* Nested Routes will be rendered here */}
+    </div>
+  );
+};
+
+export default Profile;
