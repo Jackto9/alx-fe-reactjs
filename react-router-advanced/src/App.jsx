ed112321e6a2398f258f5e9af21d8204b4232411
Jackson <gidykamau@gmail.com>
2025-03-07 13:46:31 +0300

    Fix App.jsx by adding ProtectedRoute and dynamic BlogPost route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Profile from "./components/Profile";
 import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
+        
+        {/* Protected Profile Route */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/profile/*" element={<Profile />} />
+        </Route>
+
+        {/* Dynamic Blog Post Route */}
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
